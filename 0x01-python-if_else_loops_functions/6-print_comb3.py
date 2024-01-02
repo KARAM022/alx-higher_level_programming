7aea55507abfdc85fa58fbadab88f2b3d32321a4
KARAM022 <my0fav0library@gmail.com>
2024-01-03 00:28:44 +0100

    0x01

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for f in range(10):
-    for l in range(f + 1, 10):
+    for i in range(f + 1, 10):
         if (f != 8):
-            print("{:d}{:d}".format(f, l), end=", ")
+            print("{:d}{:d}".format(f, i), end=", ")
         else:
-            print("{:d}{:d}".format(f, l), end="\n")
+            print("{:d}{:d}".format(f, i), end="\n")
