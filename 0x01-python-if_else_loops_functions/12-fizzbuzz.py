0c8c1aaf7be18d0e34cfced0d98a6099f6fa03d1
KARAM022 <my0fav0library@gmail.com>
2024-01-03 00:43:10 +0100

    0x01

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-for i in range(100):
-    if (i < 99):
-        if((i % 3 == 0) and (i % 5 != 0)):
-            i = "Fizz"
-        if((i % 3 != 0) and (i % 5 == 0)):
-            i = "Buzz"
-        if((i % 3 == 0) and (i % 5 == 0)):
-            i = "FizzBuzz"
-        print("{:02d}, ".format(i), end="")
-    else:
-        print("{:02d}".format(i), end="\n")
+def fizzbuzz():
+    for i in range(100):
+        if (i < 99):
+            if((i % 3 == 0) and (i % 5 != 0)):
+                i = "Fizz"
+            if((i % 3 != 0) and (i % 5 == 0)):
+                i = "Buzz"
+            if((i % 3 == 0) and (i % 5 == 0)):
+                i = "FizzBuzz"
+            print("{:02d}, ".format(i), end="")
+        else:
+            print("{:02d}".format(i), end="\n")
