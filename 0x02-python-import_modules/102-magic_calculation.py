c07bc8eac5042de28943f753af8f5c5e893c4bac
KARAM022 <my0fav0library@gmail.com>
2024-01-05 03:54:03 +0100

    0x02

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    from magic_calculation_102 import add, sub
     if a < b:
-        c = add(a, b)
+        c = a + b
         for i in range(4, 6):
-            c = add(c, i)
+            c = c + i
         return (c)
     else:
-        return(sub(a, b))
+        return(a - b)
