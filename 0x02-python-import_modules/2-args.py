7b5a7f3930c70a1f6807840be3cf233c230ec2ad
KARAM022 <my0fav0library@gmail.com>
2024-01-05 03:23:27 +0100

    0x02

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
         print("1 argument:")
     else:
         print("{} arguments:".format(len(sys.argv) - 1))
-    for i in range(len(sys.argv)):
+    for i in range(len(sys.argv) - 1):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
