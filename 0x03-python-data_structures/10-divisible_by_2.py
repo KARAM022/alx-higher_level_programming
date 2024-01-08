97830952c19b4bb479897b48f4aaa22be9b675cb
KARAM022 <my0fav0library@gmail.com>
2024-01-08 22:54:09 +0100

    0x03

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,11 @@
-#\!/usr/bin/python3
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    j = 0
+    res = []
+    for i in my_list:
+        if i % 2 == 0:
+            res.append(True)
+        else:
+            res.append(False)
+        j += 1
+    return res
