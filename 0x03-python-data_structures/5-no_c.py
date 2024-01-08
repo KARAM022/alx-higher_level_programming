206d4795dcec33cdf9471a66e5d054ac16c78709
KARAM022 <my0fav0library@gmail.com>
2024-01-08 23:18:26 +0100

    0x03

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if not my_string:
-        return None
     strnew = ""
     for i in my_string:
         if i != "c" and i != "C":
             strnew += i
-    return(strnew)
+    return (strnew)
