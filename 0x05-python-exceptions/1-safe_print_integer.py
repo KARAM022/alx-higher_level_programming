ce51e7c3286844e38a14fb08d2adf461cb49b907
KARAM022 <my0fav0library@gmail.com>
2024-01-23 01:19:22 +0100

    0x05

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        print('{:d}'.format(value))
+        return True
+    except (TypeError, ValueError):
+        return False
