3c793b8f47af0b2a44885ecc407278281599ffb8
KARAM022 <my0fav0library@gmail.com>
2024-01-23 18:05:39 +0100

    0x06

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''square'''
+
+
+class Square:
+    '''Square class'''
+
+    def __init__(self, size=0):
+        '''constructor.
+
+        args:
+            size: size
+        '''
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    def area(self):
+        '''current square area.'''
+        return self.__size ** 2
