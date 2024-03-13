c581a2230591996d5e65dc385fbe0ae0fa9e7384
KARAM022 <my0fav0library@gmail.com>
2024-03-13 15:27:20 +0000

    0x0D

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,4 @@
 -- 5. Full description
-SELECT column_name, data_type, character_maximum_length
+SELECT table_name, column_name, data_type, character_maximum_length
 FROM information_schema.columns
 WHERE table_schema = 'hbtn_0c_0' AND table_name = 'first_table';
