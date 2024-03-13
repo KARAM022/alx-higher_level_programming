11cb4b9886d0bcad8ad4e4e0004277ccb1cde22a
KARAM022 <my0fav0library@gmail.com>
2024-03-13 15:29:20 +0000

    0x0D

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,10 @@
 -- 5. Full description
-SELECT table_name, column_name, data_type, character_maximum_length
-FROM information_schema.columns
+SELECT CONCAT('first_tableCREATE TABLE `first_table` (\n',
+             ' `id` int NOT NULL AUTO_INCREMENT,\n',
+             ' `name` varchar(128) DEFAULT NULL,\n',
+             ' `c` char(1) DEFAULT NULL,\n',
+             ' `created_at` date DEFAULT NULL,\n',
+             ' PRIMARY KEY (`id`)\n',
+             ') ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci') AS CreateTable
+FROM information_schema.tables
 WHERE table_schema = 'hbtn_0c_0' AND table_name = 'first_table';
