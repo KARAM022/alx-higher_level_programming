7e3a232b8aad93fa414d082490848f9bfba89798
KARAM022 <my0fav0library@gmail.com>
2024-04-08 17:56:42 +0000

    0x12

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+if (!process.argv[2]) {
+  console.log('No argument');
+} else {
+  process.argv.forEach((val, index) => {
+    if (index >= 2) {
+      console.log(val);
+    }
+  })
+}
