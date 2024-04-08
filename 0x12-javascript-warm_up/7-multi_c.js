58eed89a628c7efbec5912c565f0ea915dbd59e5
KARAM022 <my0fav0library@gmail.com>
2024-04-08 21:40:15 +0000

    0x12

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-if (!isNaN(parseInt(process.argv[2])) && parseInt(process.argv[2]) > 0) {
+if (!isNaN(parseInt(process.argv[2])) && parseInt(process.argv[2])) {
   for (let i = 0; i < parseInt(process.argv[2]); i++) {
     console.log('C is fun');
   }
