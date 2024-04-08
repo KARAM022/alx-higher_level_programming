2ea26df3d126937d09941b76ed879e0eddd88247
KARAM022 <my0fav0library@gmail.com>
2024-04-08 18:07:59 +0000

    0x12

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 
-const arg = process.argv[2];
-
-if (!isNaN(parseInt(arg))) {
-  console.log(`My number: ${parseInt(arg)}`);
+if (!isNaN(parseInt(process.argv[2]))) {
+  console.log(`My number: ${parseInt(process.argv[2])}`);
 } else {
   console.log("Not a number");
 }
