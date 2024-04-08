e77902a1d6f63a865c20611b5190ac1b0c7387a9
KARAM022 <my0fav0library@gmail.com>
2024-04-08 17:38:00 +0000

    0x12

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,10 +2,8 @@
 
 if (process.argv.length == 2) {
   console.log('No argument');
+} else if (argv.length == 3) {
+    console.log('Argument found');
 } else {
-  if (argv.length == 3) {
     console.log('Argument found');
-  } else {
-    console.log('Argument found');
-  }
 }
